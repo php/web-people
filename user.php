74281089c0ff14c804bb99a818cb68a791772a5f
Dejan Marjanovic <dejan.marjanovic@gmail.com>
2013-01-25 22:14:20 +0100

    Update user.php
    Remove Gravatar image extension (makes some avatars not to display properly (default displayed)) and force PG rating (may contain rude gestures, provocatively dressed individuals, the lesser swear words, or mild violence) or better (G).

--- user.php
+++ user.php
@@ -20,7 +20,7 @@ $location = isset($PEAR["long"], $PEAR["lat"]) ? $PEAR["lat"] . ", " . $PEAR["lo
 <div about="#me" typeof="foaf:Person" id="profile">
 <?php
 if ($email) {
-    echo '<span rel="foaf:img"><img rel="foaf:img" src="http://www.gravatar.com/avatar/', md5($email), '.jpg" alt="Picture of ', $NFO["name"], '" height="80" width="80" /></span>';
+    echo '<span rel="foaf:img"><img rel="foaf:img" src="http://www.gravatar.com/avatar/', md5($email), '?r=pg" alt="Picture of ', $NFO["name"], '" height="80" width="80" /></span>';
 }
 ?>
 	<dl>

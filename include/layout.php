c88e67f0a9118fa05fb272ef5ed05a8d1291b3b3
Hannes Magnusson <bjori@mongodb.com>
2013-12-12 18:07:35 -0800

    Use the layout from the new shared module

--- include/layout.php
+++ include/layout.php
@@ -2,11 +2,25 @@
 /* $Id$ */
 
 function site_header($title) {
-    include dirname(__FILE__) . "/header.inc";
+    $SUBDOMAIN = "people";
+    $TITLE = "Header";
+    $LINKS = array(
+        array("href" => "/user.php", "text" => "Users"),
+        array("href" => "https://master.php.net/manage/users.php", "text" => "Master"),
+    );
+    $CSS[] = "/shared/styles/user-autocomplete.css";
+    $SEARCH = array("method" => "get", "action" => "user.php", "placeholder" => "Search profiles", "name" => "username");
+    include dirname(__FILE__) . "/../shared/templates/header.inc";
 }
 
 function site_footer() {
-    include dirname(__FILE__) . "/footer.inc";
+    $JS = array(
+        "//ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js",
+        "//people.php.net/js/jquery.autocomplete.min.js",
+        "//people.php.net/js/userlisting.php",
+        "//people.php.net/js/search.js",
+    );
+    include dirname(__FILE__) . "/../shared/templates/footer.inc";
 }
 
 function site_panel($data) {
